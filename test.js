1cb693d8b5c3fea04619dfdc402672f3f22fa75a
Yangwoo Kim <79357990+yangwookim2@users.noreply.github.com>
2022-08-09 00:51:13 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+
+
+
+
+var picker = document.getElementById("picker");
+window.onmousemove = function(e)
+{
+    picker.style.left = e.clientX + "px";
+    picker.style.top = e.clientY + "px";
+    margin-left: 100px;
+}
